903d76284a071770f39fce12e87c64135842c791
yu-11-22 <willy24692485@gmail.com>
2022-04-20 16:40:55 +0800

    樣式修改第一版

--- index.php
+++ index.php
@@ -6,19 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="bootstrap-4.6.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 
-<body>
+<body style="background-color:#8dedeb;">
     <div>
-        <h1>php互動式程式設計作業</h1>
+        <h1 style="text-align: center;">php互動式程式設計作業</h1>
     </div>
-    <div>
-        <a href="example01.php" target="main">題目一:亂數陣列排序</a>
-        <a href="example02.php" target="main">題目二:阿拉伯數字轉成國字</a>
-        <a href="example03.php" target="main">題目三:尋找質數程式</a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 offset-1"><a href="example01.php" target="main" class="btn btn-blue">題目一:亂數陣列排序</a></div>
+            <div class="col-md-3 offset-1"><a href="example02.php" target="main" class="btn btn-blue">題目二:阿拉伯數字轉成國字</a></div>
+            <div class="col-md-3 offset-1"><a href="example03.php" target="main" class="btn btn-blue">題目三:尋找質數程式</a></div>
+        </div>
     </div>
-
-    <iframe frameborder="0" id="main" name="main" width="1024px" height="768px"></iframe>
+    <iframe frameborder="0" id="main" name="main" width="100%" height="768px"></iframe>
 </body>
+<!-- bootstrap JS -->
+<script type="text/javascript" src="bootstrap-4.6.1-dist/js/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap-4.6.1-dist/js/bootstrap.bundle.js"></script>
 
 </html>
