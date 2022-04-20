af828784401b6b8acdca39020172c8f1ece3f373
yu-11-22 <willy24692485@gmail.com>
2022-04-20 15:54:29 +0800

    example01的內容第二版已完成

--- example01.php
+++ example01.php
@@ -24,41 +24,57 @@
     <?php
     ini_set("error_reporting", "E_ALL & ~E_NOTICE");
     if (isset($_POST['MM_form']) and $_POST['MM_form'] == 'form1') {
+        if ($_POST['RadioGroup1'] == '1') {
+            $start = microtime();
+            echo sortArr1();
+            $end = microtime();
+            $showData = ($end - $start) * 1000;
+            echo "<br><p>所需時間:" . $showData . "ms</p>";
+        } elseif ($_POST['RadioGroup1'] == '2') {
+            $start = microtime();
+            echo sortArr2();
+            $end = microtime();
+            $showData = ($end - $start) * 1000;
+            echo "<br><p>所需時間:" . $showData . "ms</p>";
+        }
+    }
+    function sortArr1()
+    {
+        $newData = array();
         for ($i = 0; $i < 5000; $i++) {
             $data[$i] = rand(1, 100000);
         }
-        echo "<p>排序前內容:";
+        $size = count($data);
+        echo "排列前的順序為:";
         print_r($data);
-        echo "</p>";
-
-        $start = microtime();
-        $new = bubble_sort($data, $_POST['RadioGroup1']);
-        $end = microtime();
-        $showData = $end - $start;
-
-        echo "<p>排序完成內容:";
-        print_r($new);
-        echo "</p>";
-        echo "<p>所需時間:" . ($showData * 1000) . "ms</p>";
-    }
-    function bubble_sort($arr, $mod = 1)
-    {
-        $size = count($arr) - 1;
+        echo "<br>";
         for ($i = 0; $i < $size; $i++) {
-            for ($j = 0; $j < $size; $j++) {
-                $k = $j + 1;
-                if ($mod == 1) {
-                    if ($arr[$k] < $arr[$j]) {
-                        list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
-                    }
-                } else {
-                    if ($arr[$k] > $arr[$j]) {
-                        list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
-                    }
-                }
-            }
+            $minD = min($data);
+            $key = array_search($minD, $data);
+            array_push($newData, $minD);
+            unset($data[$key]);
         }
-        return $arr;
+        echo "排列後的順序為:";
+        print_r($newData);
+    }
+    function sortArr2()
+    {
+        $newData = array();
+        for ($i = 0; $i < 5000; $i++) {
+            $data[$i] = rand(1, 100000);
+        }
+        $size = count($data);
+        echo "排列前的順序為:";
+        print_r($data);
+        echo "<br>";
+        for ($i = 0; $i < $size; $i++) {
+            $maxD = max($data);
+            $key = array_search($maxD, $data);
+            array_push($newData, $maxD);
+            unset($data[$key]);
+        }
+        echo "排列後的順序為:";
+        print_r($newData);
     }
     ?>
 </body>
